ba7af93886b7d54f89e6c3049b57b5536c3a4620
sangnguyen0403 <sangnguyen04032002@gmail.com>
2023-05-12 14:20:17 +0700

    Fixx UI

--- src/App.js
+++ src/App.js
@@ -1,81 +1,20 @@
 import './App.css';
 import { Box, Button, Center, Flex, HStack, Heading, Image, Input, Select, VStack, useMultiStyleConfig } from '@chakra-ui/react';
-import image from "./images/background.svg"
+
+import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Task1 from './components/Task1';
 
 
 
 function App() {
-  const handleUploadImage = (file) => {
-    console.log(file)
-  }
   return (
-    <Center
-      bgImage={image}
-      bgPosition="center"
-      bgRepeat="no-repeat"
-      width="100vw"
-      height="100vh"
-    >
-      <VStack w='59%' h='100%' pt='10' spacing={7}>
-        <VStack w='100%' >
-          <Heading as='h1' size='2xl' letterSpacing='wider' noOfLines={1}>Flowers</Heading>
-          <Heading as='h4' size='md' color='#777777'>Find the bouquet you want!</Heading>
-        </VStack>
-        <Center w='100%'>
-          <HStack w='100%' justifyContent='space-around' alignItems='center'>
-            <VStack>
-              <input
-                type="file"
-                id="files"
-                className="upload_btn"
-                onChange={event => {
-                  handleUploadImage(
-                    event.target.files[0]
-                  );
-                }}
-              />
-              <label
-                htmlFor="files"
-                className="upload_label"
-              >
-                Select Image
-              </label>
-            </VStack>
-            <HStack w='50%'>
-              <Select bg='#FFFFFF' color='#060825' boxShadow='lg' py='6' rounded='md' placeholder="Select option">
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </Select>
-              <Button bg='#060825' color='#FAFAFA' px='36px' py='12px' borderRadius='10px' boxShadow='lg' rounded='md'>Select</Button>
-            </HStack>
-          </HStack>
-        </Center>
-        <HStack w='100%' justifyContent='space-around' alignItems='center'>
-          
-          <Center w='45%'>
-            <Image
-              // boxSize="50%"
-              // objectFit="cover"
-              src={image}
-              alt="Dan Abramov"
-              borderRadius='20px'
-            // mt='2'
-            />
-          </Center>
-          <Center w='45%'>
-            <Image
-              // boxSize="50%
-              // objectFit="cover"
-              src={image}
-              alt="Dan Abramov"
-              borderRadius='20px'
-            // mt='2'
-            />
-          </Center>
-        </HStack>
-      </VStack>
-    </Center>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/task1' element={<Task1 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import { Center, HStack, Heading, VStack, Image, Select, Button, Flex, Divider } from '@chakra-ui/react';
+import '../App.css';
+import image from "../images/background.svg"
+import { Link } from 'react-router-dom';
+const Home = () => {
+    const handleUploadImage = (file) => {
+        console.log(file)
+    }
+    return (
+        <Center
+            bgImage={image}
+            bgPosition="center"
+            bgRepeat="no-repeat"
+            width="100vw"
+            height="100vh"
+        >
+            <VStack w='59%' h='100%' pt='10' spacing={10}>
+                <VStack w='100%' gap={6}>
+                    <Heading as='h1' size='3xl' letterSpacing='wider' noOfLines={1}>FLOWERS</Heading>
+                    <Heading as='h4' size='md' color='#777777'>Find the bouquet you want!</Heading>
+                </VStack>
+                <HStack w='100%' justifyContent='space-around' alignItems='center'>
+                    <Link to='/task1'><Button px='50px' py='36px' borderRadius='10px' bg='#060825' color='#FAFAFA' fontWeight='500' fontSize='30px' boxShadow='lg' rounded='md' _hover={{bg: '#FFFFFF', border: '1px solid #060825', color: '#060825'}}>Task 1</Button></Link>
+                    <Link to='/task1'><Button px='50px' py='36px' borderRadius='10px' bg='#060825' color='#FAFAFA' fontWeight='500' fontSize='30px' boxShadow='lg' rounded='md' _hover={{bg: '#FFFFFF', border: '1px solid #060825', color: '#060825'}}>Task 2</Button></Link>
+                </HStack>
+
+            </VStack>
+        </Center>
+    )
+}
+
+
+export default Home;
